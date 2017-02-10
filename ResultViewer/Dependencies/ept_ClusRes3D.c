ad52b50eb935c04f5d0f96ac368c7c636244893b
Mensen <research.mensen@gmail.com>
2017-02-10 11:50:37 +0100

    fixed bug using mex for cluster statistics

--- ResultViewer/Dependencies/ept_ClusRes3D.c
+++ ResultViewer/Dependencies/ept_ClusRes3D.c
@@ -1,16 +1,16 @@
-// This file is part of the program ept_TFCE.
-// ept_TFCE is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// ept_TFCE is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License
-// along with ept_TFCE.  If not, see <http://www.gnu.org/licenses/>.
+/* This file is part of the program ept_TFCE.
+ * ept_TFCE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * ept_TFCE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with ept_TFCE.  If not, see <http://www.gnu.org/licenses/>.
 
-/*
+ *
  * Christian Gaser
  *
  * Pau Coma 09.12.2010
@@ -49,20 +49,16 @@ void tfce_thread(double *inData, double *outData, double *ChN, double *thresh, c
    int i, t, f, ti, tt, tf, maxt, mint, maxf, minf, temp, growingInd, growingCur, ChCurr, idx, p=0, n=0;
    int numVoxels = dims[0] * dims[1] * dims[2];
    char* flagUsed;
-   //#-short* growing;
+   /*#-short* growing; */
    short* grow_i;
    short* grow_t;
    short* grow_f;
-   //-#
    
    
    flagUsed = (char*)malloc(numVoxels*sizeof(char));
-   //#-growing  = (short*)malloc(numVoxels*3*sizeof(short));
    grow_i  = (short*)malloc(numVoxels*sizeof(short));
    grow_t  = (short*)malloc(numVoxels*sizeof(short));
-   grow_f  = (short*)malloc(numVoxels*sizeof(short));
-   //-#
-   
+   grow_f  = (short*)malloc(numVoxels*sizeof(short));   
 
     for (temp = 0; temp < numVoxels; ++temp) flagUsed[temp] = 0;
     
@@ -72,24 +68,24 @@ void tfce_thread(double *inData, double *outData, double *ChN, double *thresh, c
 		{
 			for (i = 0; i < dims[0]; ++i)
 			{
-				// temp is the current point in the grid
+				/* temp is the current point in the grid */
 				temp = (f*dims[1]*dims[0]) + (t*dims[0]) + i;
 				
-				// Check if this point has been seen (flagUsed) and whether its over the threshold
+				/* Check if this point has been seen (flagUsed) and whether its over the threshold */
 				if (!flagUsed[temp] && inData[temp] >= thresh[0])
 				{
-				    // mexPrintf("\nPositive Values... \n");
-					// mexPrintf("Looking at %f \n", inData[temp]);
-					// mexPrintf("t is %d \n", t);
-					// mexPrintf("f is %d \n", f);
-					// mexEvalString("drawnow;"); // to dump string.
+				    /* mexPrintf("\nPositive Values... \n");
+					/* mexPrintf("Looking at %f \n", inData[temp]);
+					/* mexPrintf("t is %d \n", t);
+					/* mexPrintf("f is %d \n", f);
+					/* mexEvalString("drawnow;"); */
 								
-					// make the flagUsed so that algorithm doesn't visit this point again
+					/* make the flagUsed so that algorithm doesn't visit this point again */
 					flagUsed[temp] = 1;
 					growingInd = 1;
 					growingCur = 0;
 					
-					// Define the current coordinates to continue with
+					/* Define the current coordinates to continue with */
 					grow_i[0]=i;
 					grow_t[0]=t;
 					grow_f[0]=f;
@@ -97,17 +93,17 @@ void tfce_thread(double *inData, double *outData, double *ChN, double *thresh, c
 					
 					while (growingCur < growingInd)
 					{
-					   //This just limits not to overrun borders <-- in our case the zero padding
-					   //And creates a 3x3 windows of scanning for valid points (one point around the current point)
-					   //E.g. In 2 dimensions... if the current point is 2,j then maxi = Min of dimension size or 2+2
-					   //thus maxi = 4... mini = max of 0 or 2-1... so 1...
-					   //so in the next set of loops ti will look at i=1,i=2,and i=3 (not 4 because its set to < maxi
+					   /*This just limits not to overrun borders <-- in our case the zero padding
+					   /*And creates a 3x3 windows of scanning for valid points (one point around the current point)
+					   /*E.g. In 2 dimensions... if the current point is 2,j then maxi = Min of dimension size or 2+2
+					   /*thus maxi = 4... mini = max of 0 or 2-1... so 1...
+					   /*so in the next set of loops ti will look at i=1,i=2,and i=3 (not 4 because its set to < maxi */
 					   maxt = MIN(dims[1], grow_t[growingCur] + 2);
 					   maxf = MIN(dims[2], grow_f[growingCur] + 2);
 					   mint = MAX(0, grow_t[growingCur] - 1);
 					   minf = MAX(0, grow_f[growingCur] - 1);
 						
-						// start of the smaller scanning window
+						/* start of the smaller scanning window */
 						for (tf = minf; tf < maxf; ++tf)
 						{
 							for (tt = mint; tt < maxt; ++tt)
@@ -123,7 +119,7 @@ void tfce_thread(double *inData, double *outData, double *ChN, double *thresh, c
 										break;
 									}
 									
-									ChCurr = ChCurr - 1; // This is correct because we are dealing with c 0-based indexing in the next if statement
+									ChCurr = ChCurr - 1;
 									
 									temp = tf*(dims[0]*dims[1]) + (tt*dims[0]) + ChCurr;
 									
@@ -134,41 +130,34 @@ void tfce_thread(double *inData, double *outData, double *ChN, double *thresh, c
 										grow_t[growingInd] = tt;
 										grow_f[growingInd] = tf;
 										growingInd++;
-										//Here the growing index increases everytime we find a value above the threshhold
-										//This grows depending on the origin of our supporting weight which is the outer loop
-										//-#
 									}
 								}	
 							}
 						}
-					   // GrowingCur increases one and thus looks at the next point that was found in the previous loop
+					   /* GrowingCur increases one and thus looks at the next point that was found in the previous loop */
 					   growingCur++;
 
 					}
-					// Reset back to 0 so that when the next "while loop" runs it adds the value to each point found
+					/* Reset back to 0 so that when the next "while loop" runs it adds the value to each point found */
 					growingCur = 0;
 								
 					valToAdd = p;
 					
-					// Adds the valToAdd to the points that it found in that cluster
 					while (growingCur < growingInd)
 					{
 						outData[(grow_f[growingCur]*dims[1]*dims[0]) + (grow_t[growingCur]*dims[0]) + grow_i[growingCur]] += valToAdd;
-					   // GrowingCur adds one so that next coordinate is added until growingCurr is less than the number of points found
+
 					   growingCur++;
 					}
 				}
 				
-				// reset temp for negative values
 				temp = (f*dims[1]*dims[0]) + (t*dims[0]) + i;
 				if (!flagUsed[temp] && -inData[temp] >= thresh[0])
 				{
-					// make the flagUsed so that algorithm doesn't visit this point again
 					flagUsed[temp] = 1;
 					growingInd = 1;
 					growingCur = 0;
 					
-					// Define the current coordinates to continue with
 					grow_i[0]=i;
 					grow_t[0]=t;
 					grow_f[0]=f;
@@ -176,17 +165,11 @@ void tfce_thread(double *inData, double *outData, double *ChN, double *thresh, c
 
 					while (growingCur < growingInd)
 					{
-					   //This just limits not to overrun borders <-- in our case the zero padding
-					   //And creates a 3x3 windows of scanning for valid points (one point around the current point)
-					   //E.g. In 2 dimensions... if the current point is 2,j then maxi = Min of dimension size or 2+2
-					   //thus maxi = 4... mini = max of 0 or 2-1... so 1...
-					   //so in the next set of loops ti will look at i=1,i=2,and i=3 (not 4 because its set to < maxi
 					   maxt = MIN(dims[1], grow_t[growingCur] + 2);
 					   maxf = MIN(dims[2], grow_f[growingCur] + 2);
 					   mint = MAX(0, grow_t[growingCur] - 1);
 					   minf = MAX(0, grow_f[growingCur] - 1);
 						
-						// start of the smaller scanning window
 						for (tf = minf; tf < maxf; ++tf)
 						{
 							for (tt = mint; tt < maxt; ++tt)
@@ -201,7 +184,7 @@ void tfce_thread(double *inData, double *outData, double *ChN, double *thresh, c
 										break;
 									}
 									
-									ChCurr = ChCurr - 1; // This is correct because we are dealing with c 0-based indexing in the next if statement
+									ChCurr = ChCurr - 1;
 									
 									temp = tf*(dims[0]*dims[1]) + (tt*dims[0]) + ChCurr;
 									if (!flagUsed[temp] && -inData[temp] >= thresh[0])
@@ -217,16 +200,13 @@ void tfce_thread(double *inData, double *outData, double *ChN, double *thresh, c
 						}
 					    growingCur++;
 					}
-					// Reset back to 0 so that when the next "while loop" runs it adds the value to each point found
 					growingCur = 0;
 								
 					valToAdd = n;
 					
-					// Adds the valToAdd to the points that it found in that cluster
 					while (growingCur < growingInd)
 					{
 						outData[(grow_f[growingCur]*dims[1]*dims[0]) + (grow_t[growingCur]*dims[0]) + grow_i[growingCur]] -= valToAdd;
-						// GrowingCur adds one so that next coordinate is added until growingCurr is less than the number of points found
 						growingCur++;
 					}
 				}
@@ -266,10 +246,10 @@ thresh = (double*)mxGetPr(prhs[2]);
 ndim = mxGetNumberOfDimensions(prhs[0]);
 ndim2 = mxGetNumberOfDimensions(prhs[1]);
 if (ndim!=3){
-	mxErrMsgTxt("Data input should be 3D: Channel*Frequency*Time");
+	mexErrMsgTxt("Data input should be 3D: Channel*Frequency*Time");
 }
 else if (ndim2!=2){
-	mxErrMsgTxt("Channel*Neighbour file should be 2D");
+	mexErrMsgTxt("Channel*Neighbour file should be 2D");
 }
 	
 dims = mxGetDimensions(prhs[0]);
